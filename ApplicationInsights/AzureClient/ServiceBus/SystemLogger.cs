13e67a00efcb5baf84760d4c3a004609905b0270
CraigGraw <craig@graw.co.uk>
2022-04-11 13:01:30 +0100

    Fix log
    Fix

--- ApplicationInsights/AzureClient/ServiceBus/SystemLogger.cs
+++ ApplicationInsights/AzureClient/ServiceBus/SystemLogger.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using log4net;
 
@@ -9,28 +11,44 @@ namespace AzureClient.ServiceBus
         private ILog _adaptee;
 
         public void Debug(string message, Exception exception = null, [CallerFilePath] string filePath = "", [CallerMemberName] string methodName = "")
-        {   
+        {
+            Log(filePath);
             _adaptee.Debug(message, exception);
         }
 
         public void Error(string message, Exception exception = null, [CallerFilePath] string filePath = "", [CallerMemberName] string methodName = "")
         {
+            Log(filePath);
             _adaptee.Error(message, exception);
         }
 
         public void Fatal(string message, Exception exception = null, [CallerFilePath] string filePath = "", [CallerMemberName] string methodName = "")
-        {   
+        {
+            Log(filePath);
             _adaptee.Fatal(message, exception);
         }
 
         public void Info(string message, Exception exception = null, [CallerFilePath] string filePath = "", [CallerMemberName] string methodName = "")
         {
+            Log(filePath);
             _adaptee.Info(message, exception);
         }
 
         public void Warn(string message, Exception exception = null, [CallerFilePath] string filePath = "", [CallerMemberName] string methodName = "")
         {
+            Log(filePath);
             _adaptee.Warn(message, exception);
+        }
+
+        private void Log(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                throw new ArgumentNullException(nameof(filePath));
+            }
+
+            string className = Path.GetFileNameWithoutExtension(filePath);
+            _adaptee = LogManager.GetLogger(Assembly.GetExecutingAssembly(), className);
         }
     }
 }
